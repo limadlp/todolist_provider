33c3b05c00586a12b55e27e37abf5a3e5b9f0bfd
Yami <dlplima@hotmail.com>
2021-11-09 23:00:41 -0300

    authstatechanges

--- lib/app/modules/auth/register/register_page.dart
+++ lib/app/modules/auth/register/register_page.dart
@@ -39,7 +39,6 @@ class _RegisterPageState extends State<RegisterPage> {
       context: context,
       successCallback: (notifier, listenerInstance) {
         listenerInstance.dispose();
-        Navigator.of(context).pop();
       },
     );
   }
