9004c6388600951e07682c199383e9ec932321e7
Yami <dlplima@hotmail.com>
2021-11-11 23:03:09 -0300

    task filters

--- lib/app/modules/home/widgets/todo_card_filter.dart
+++ lib/app/modules/home/widgets/todo_card_filter.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'package:todo_list_provider/app/core/ui/theme_extensions.dart';
 import 'package:todo_list_provider/app/models/task_filter_enum.dart';
 import 'package:todo_list_provider/app/models/total_tasks_model.dart';
+import 'package:todo_list_provider/app/modules/home/home_controller.dart';
 
 class TodoCardFilter extends StatelessWidget {
   final String label;
@@ -31,59 +33,67 @@ class TodoCardFilter extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      constraints: const BoxConstraints(
-        minHeight: 120,
-        maxWidth: 150,
-      ),
-      padding: const EdgeInsets.all(20),
-      margin: const EdgeInsets.only(
-        right: 10,
-      ),
-      decoration: BoxDecoration(
-        color: selected ? context.primaryColor : Colors.white,
-        border: Border.all(
-          width: 1,
-          color: Colors.grey.withOpacity(0.8),
+    return InkWell(
+      onTap: () {
+        context.read<HomeController>().findTasks(filter: taskFilter);
+        //selected = true;
+      },
+      borderRadius: BorderRadius.circular(30),
+      child: Container(
+        constraints: const BoxConstraints(
+          minHeight: 120,
+          maxWidth: 150,
         ),
-        borderRadius: BorderRadius.circular(30),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            '${totalTasksModel?.totalTasks ?? 0} TASKS',
-            style: context.titleStyle.copyWith(
-              fontSize: 10,
-              color: selected ? Colors.white : Colors.grey,
-            ),
+        padding: const EdgeInsets.all(20),
+        margin: const EdgeInsets.only(
+          right: 10,
+        ),
+        decoration: BoxDecoration(
+          color: selected ? context.primaryColor : Colors.white,
+          border: Border.all(
+            width: 1,
+            color: Colors.grey.withOpacity(0.8),
           ),
-          Text(
-            label,
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
-              color: selected ? Colors.white : Colors.black,
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              '${totalTasksModel?.totalTasks ?? 0} TASKS',
+              style: context.titleStyle.copyWith(
+                fontSize: 10,
+                color: selected ? Colors.white : Colors.grey,
+              ),
             ),
-          ),
-          TweenAnimationBuilder<double>(
-            builder: (context, value, child) {
-              return LinearProgressIndicator(
-                backgroundColor:
-                    selected ? context.primaryColorLight : Colors.grey.shade300,
-                valueColor: AlwaysStoppedAnimation<Color>(
-                  selected ? Colors.white : context.primaryColor,
-                ),
-                value: value,
-              );
-            },
-            tween: Tween(
-              begin: 0.0,
-              end: _getPercentFinish(),
+            Text(
+              label,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: selected ? Colors.white : Colors.black,
+              ),
             ),
-            duration: const Duration(seconds: 1),
-          ),
-        ],
+            TweenAnimationBuilder<double>(
+              builder: (context, value, child) {
+                return LinearProgressIndicator(
+                  backgroundColor: selected
+                      ? context.primaryColorLight
+                      : Colors.grey.shade300,
+                  valueColor: AlwaysStoppedAnimation<Color>(
+                    selected ? Colors.white : context.primaryColor,
+                  ),
+                  value: value,
+                );
+              },
+              tween: Tween(
+                begin: 0.0,
+                end: _getPercentFinish(),
+              ),
+              duration: const Duration(seconds: 1),
+            ),
+          ],
+        ),
       ),
     );
   }
